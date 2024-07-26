c453bc34f6f239fd9e4f4df491a3d0f09c1a49ac
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-07-27 01:13:01 +0300

    add splash images in splash view and make it navigate after 3 second

---
+++ lib/modules/quran/quran_view.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class QuranView extends StatelessWidget {
+  const QuranView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/modules/splash/splash_view.dart
+++ lib/modules/splash/splash_view.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
   static const String routeName = "splash";
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    Future.delayed(
+      const Duration(
+        seconds: 3,
+      ),
+      () => Navigator.of(context).pushReplacementNamed(routeName),
+    );
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+            'assets/images/splash.png',
+          ),
+          fit: BoxFit.fill,
+        ),
+      ),
+    );
   }
 }
