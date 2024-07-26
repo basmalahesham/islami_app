70f0da17c06f4b1971ae0dd258c48fbdafea59a0
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-07-27 01:31:37 +0300

    handling abbBarTheme in main

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,15 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          centerTitle: true,
+          titleTextStyle: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
       initialRoute: SplashView.routeName,
       routes: {
