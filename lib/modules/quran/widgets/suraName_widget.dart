5bf7d707676fd3b2cdba6a1af4d4642afb5d1796
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-07-29 06:53:05 +0300

    create suraName list and listView.builder in quranView create suraName_widget.dart

---
+++ lib/modules/quran/widgets/suraName_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SuraNameWidget extends StatelessWidget {
+  const SuraNameWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
