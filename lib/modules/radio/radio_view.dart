6d08badce4bfd8d6fbbd07c14e452e2ef27df616
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-07-31 21:54:39 +0300

    add future builder

--- lib/modules/radio/radio_view.dart
+++ lib/modules/radio/radio_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:islami_app/modules/radio/radio_service.dart';
 
 class RadioView extends StatelessWidget {
   const RadioView({super.key});
@@ -8,27 +9,30 @@ class RadioView extends StatelessWidget {
     var theme = Theme.of(context);
     return Container(
       width: double.infinity,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Spacer(),
-          Image.asset(
-            'assets/images/radio_image.png',
-            width: 250,
-          ),
-          Spacer(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Icon(Icons.skip_previous,size: 35,color: theme.canvasColor,),
-              Icon(Icons.play_arrow,size: 35,color: theme.canvasColor,),
-              Icon(Icons.stop,size: 35,color: theme.canvasColor,),
-              Icon(Icons.skip_next,size: 35,color: theme.canvasColor,),
-            ],
-          ),
-          Spacer(),
-        ],
+      child: FutureBuilder(
+        future: getRadio(),
+        builder: (context,snapshot) =>Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Spacer(),
+            Image.asset(
+              'assets/images/radio_image.png',
+              width: 250,
+            ),
+            Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(Icons.skip_previous,size: 35,color: theme.canvasColor,),
+                Icon(Icons.play_arrow,size: 35,color: theme.canvasColor,),
+                Icon(Icons.stop,size: 35,color: theme.canvasColor,),
+                Icon(Icons.skip_next,size: 35,color: theme.canvasColor,),
+              ],
+            ),
+            Spacer(),
+          ],
+        ),
       ),
     );
   }
